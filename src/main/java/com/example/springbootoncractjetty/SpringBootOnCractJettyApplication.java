e11bf9c2900e2b6c32a9ce87e549c1722b3234ec
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-10-28 12:54:02 +0200

    initial commit

---
+++ src/main/java/com/example/springbootoncractjetty/SpringBootOnCractJettyApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootoncractjetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOnCractJettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOnCractJettyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootoncractjetty/SpringBootOnCractJettyApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.springbootoncractjetty;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOnCractJettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
