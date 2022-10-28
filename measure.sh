1df7b04e0e46e78349746b1ec728c911f974f015
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2022-10-28 14:15:48 +0200

    ..

--- measure.sh
+++ measure.sh
@@ -1,7 +1,8 @@
-perf stat $DEMO_COMMAND &
-export PERF_PID=$!
+START=$(date +%s.%N)
 while [ "$(curl -s -o /dev/null -L -w ''%{http_code}'' http://localhost:8080/hello)" != "200" ];
   do sleep 0.001;
 done
-export MY_PID="$(pgrep -P $PERF_PID)"
-kill $MY_PID
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo "Measured"
+echo DIFF
